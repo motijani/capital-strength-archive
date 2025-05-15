64af1a9eae235db14cb87c879eecd5661ec006aa
MOMOMAZED <38228412+MOMOMAZED@users.noreply.github.com>
2025-05-15 02:46:23 -0400

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/capital-strength-archive",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
